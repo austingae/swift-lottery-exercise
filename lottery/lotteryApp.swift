d5661a089df80762032a2bdc0c0834e771b1b83b
Austin Gae <austingae@gmail.com>
2022-09-05 11:15:02 -0400

    Added assets

---
+++ lottery/lotteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  lotteryApp.swift
+//  lottery
+//
+//  Created by Austin Gae on 9/5/22.
+//
+
+import SwiftUI
+
+@main
+struct lotteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
