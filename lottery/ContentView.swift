74dac6071803a06271ef721b6e1e182825b16473
Austin Gae <austingae@gmail.com>
2022-09-05 11:59:48 -0400

    I created the functions randomlySelectFruits() and check() that randomly selects fruits for the three slots and checks whether the fruits in three slots are equal, respectively.

--- lottery/ContentView.swift
+++ lottery/ContentView.swift
@@ -8,10 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var credits = 1000
+    @State private var credits = 0
     @State private var leftSlot = "apple"
     @State private var middleSlot = "apple"
     @State private var rightSlot = "apple"
+    @State private var gameStatus = ""
+    
+    func randomlySelectFruits() {
+        for i in 1...3 {
+            let options = ["apple", "cherry", "star"]
+            let randomIndex = Int.random(in: 0..<3)
+            let selectedOption = options[randomIndex]
+            
+            if (i == 1) {
+                leftSlot = selectedOption
+            }
+            else if (i == 2) {
+                middleSlot = selectedOption
+            }
+            else if (i == 3) {
+                rightSlot = selectedOption
+            }
+        }
+    }
+    
+    func check(left: String, middle: String, right: String) {
+        if (left == middle && left == right && middle == right) {
+            credits = credits + 100
+        }
+        else {
+            credits = credits - 25
+            
+            if (credits < 0) {
+                gameStatus = "Game Over"
+            }
+        }
+    }
     
     var body: some View {
         VStack {
@@ -46,7 +78,11 @@ struct ContentView: View {
             Spacer()
             
             Button(action: {
+                //randomly select the fruits for the three slots
+                randomlySelectFruits()
                 
+                //check if the fruits in the three slots are equal. If equal, then +100 credits. If not equal, then -25 credits.
+                check(left: leftSlot, middle: middleSlot, right: rightSlot)
             }, label: {
                 Text("Spin")
             }).padding(.all, 16.0)
@@ -55,6 +91,8 @@ struct ContentView: View {
             
             
             Spacer()
+            
+            Text(gameStatus)
         }
     }
 }
