0f47311e6e72ef89f1587b7911f8c3e975a27b04
Austin Gae <austingae@gmail.com>
2022-09-05 11:24:23 -0400

    I created the user interface statically

--- lottery/ContentView.swift
+++ lottery/ContentView.swift
@@ -9,8 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Spacer()
+            Text("Fruit Roll Lottery")
+            Spacer()
+            Text("Credits: 1000")
+            Spacer()
+            HStack {
+                Spacer()
+                Image("apple")                  .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 200)
+                
+                Spacer()
+                
+                Image("cherry")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 200)
+                
+                Spacer()
+                
+                Image("star")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 200)
+                Spacer()
+            }
+            Button(action: {
+                
+            }, label: {
+                Text("Spin")
+            })
+            
+            Spacer()
+        }
     }
 }
 
